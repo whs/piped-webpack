9dcc779c02cfef15546adb525ae4c65b169aeb7f
Manatsawin Hanmongkolchai <manatsawin+git@gmail.com>
2017-06-30 13:32:42 +0700

    Added tests

---
+++ test/index.js
@@ -0,0 +1,86 @@
+const expect = require('chai').expect;
+const File = require('vinyl');
+const pipedWebpack = require('../');
+
+const noop = function(){};
+
+function testFile(file){
+	let expectedResult;
+
+	if(file.basename === 'entry.js'){
+		expectedResult = true;
+	}else if(file.basename === 'entry2.js'){
+		expectedResult = false;
+	}else{
+		return expect.fail(`Unknown file ${file.basename}`);
+	}
+
+	let output = eval(file.contents.toString());
+	output(function(result){
+		expect(result).to.eql(expectedResult);
+	});
+}
+
+function assertWebpackOutput(stream, cb=noop){
+	let foundFiles = [];
+	stream.on('data', function(file){
+		foundFiles.push(file.path);
+		testFile(file);
+	})
+	stream.on('end', function(){
+		foundFiles.sort();
+		expect(foundFiles).to.eql(['deep/path/entry.js', 'deep/path/entry2.js']);
+		cb();
+	});
+}
+
+describe('PipedWebpack', function(){
+	beforeEach(function(){
+		this.config = {
+			entry: {
+				entry: [__dirname + '/../test_files/entry.js'],
+				entry2: [__dirname + '/../test_files/entry2.js'],
+			},
+			output: {
+				filename: 'deep/path/[name].js'
+			},
+			stats: 'none',
+		};
+	});
+
+	it('can compile with set entrypoints', function(cb){
+		let stream = pipedWebpack(this.config);
+		stream.end();
+		assertWebpackOutput(stream, cb);
+	});
+
+	it('can compile with generated entrypoints', function(cb){
+		delete this.config.entry;
+		let stream = pipedWebpack(this.config);
+
+		for(let file of [__dirname + '/../test_files/entry.js', __dirname + '/../test_files/entry2.js']){
+			stream.write(new File({
+				path: file
+			}));
+		}
+
+		stream.end();
+		assertWebpackOutput(stream, cb);
+	});
+
+	it('can switch plugin instance', function(){
+		let self = this;
+
+		class SpyPlugin {
+			constructor(config){
+				expect(config).to.eql(self.config);
+			}
+
+			run(){
+				return 'PASS';
+			}
+		}
+
+		expect(pipedWebpack(this.config, SpyPlugin)).to.eql('PASS');
+	});
+});
